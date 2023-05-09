16c57b8aed1374d1ef10ba497ce69565e866ff53
jonesnat <114261616+jonesnat@users.noreply.github.com>
2023-05-09 12:56:18 -0400

    Update mtwins_dataDrop_pictures_file_mover.py
    Natasha updated script to add argparse arguments - using it to move files out of dataDrop

--- python/mtwins_dataDrop_pictures_file_mover.py
+++ python/mtwins_dataDrop_pictures_file_mover.py
@@ -6,18 +6,26 @@ from os.path import join as pjoin
 from glob import glob
 from shutil import copyfile
 from shutil import move as movefile
+import argparse
+import sys
 
-# Placeholder variables for the family ID and wave.  These will eventually
-# become command line arguments to the script.
-fam  = '911'
-wave = '2'
+parser = argparse.ArgumentParser()
+
+# familyid command line input
+parser.add_argument("-f", "--familyid", help="Print family ID.")
+# wave command line input
+parser.add_argument("-w", "--wave", help="print wave (2 or 3).")
+
+# Actually parse the defined arguments
+args = parser.parse_args()
+
 
 # Directory where the original files are found
-dDrop = f'/nfs/turbo/lsa-lukehyde-secure/MTwiNS/dataDrop/Wave{wave}/{fam}/Pictures'
-
+dDrop = f'/nfs/turbo/lsa-lukehyde-secure/MTwiNS/dataDrop/Wave{args.wave}/{args.familyid}/Pictures'
 # Directory where the picture should go
-fampic_loc = f'/nfs/turbo/lsa-lukehyde-secure/MTwiNS/Data/time{wave}/family_picture'
-hair_loc = f'/nfs/turbo/lsa-lukehyde/MTwiNS/time{wave}/hair_cort'
+fampic_loc = f'/nfs/turbo/lsa-lukehyde-secure/MTwiNS/Data/time{args.wave}/family_picture'
+hair_loc = f'/nfs/turbo/lsa-lukehyde/MTwiNS/time{args.wave}/hair_cort'
+
 
 # Must change to the directory in which glob will look
 os.chdir(dDrop)
@@ -36,30 +44,30 @@ for file in sorted(pictures):
         s, pic = file.split('_')
 
         # The first two characters of the twin will be the 't[12]' that we need
-        s = s[length - 2:]
+        s = s[len(s) - 2:]
 
         # Rename hair .JPG files
-        hair_file = f'{fam}_hair_{s}.jpg'
+        file = f'{args.familyid}_hair_{s}.jpg'
 
         # Construct the real target path for hair picture files
-        new_file = pjoin(hair_loc, hair_file)
+        new_file = pjoin(hair_loc, file)
         
     elif "family" in file:
-        # Construct the real target path for family picture file        
+        # Construct the real target path for family picture file
         new_file = pjoin(fampic_loc, file)
 
     else:
         print('Something is wrong with the file name, check files in Picture folder')
-        # Need to add more to this condition 
+        # Need to add more to this condition
         sys.exit()
 
     print(f'\nMoving {old_file}\n  to {new_file}')
+    
     try:
-        copyfile(old_file, new_file)
-        # Comment or remove above line and uncomment following when ready to move.
-        # movefile(old_file, new_file)
+        movefile(old_file, new_file)
     except:
         print("Something went wrong moving", os.path.basename(old_file),
               "to", os.path.basename(new_file))
         print("Investigate and fix it.  Quitting\n")
         sys.exit()
+
